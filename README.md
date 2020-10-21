f08a4abfe5c86b48c3359f61599d5a350367d8b7
Andrew Yankowsky <andrew4world@gmail.com>
2020-10-21 11:01:19 -0400

    Created sender to modulate raise/lower hand and mute/unmute based on Zoom global keyboard shortcuts.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Zoom-Covert-Channel
+
+Sender:
+Make sure Zoom client for the has "Mute/Unmute My Audio" and "Raise\Lower Hand" keyboard shortcuts enabled globally.
+Verify shortcut for Mute/Unmute is ALT+A and Raise\Lower Hand is ALT+Y
---
+++ sender.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+
+
+if __name__ == "__main__":
+
+    # Prompts for message and converts to string of binary
+    msg = input("Enter your message: ")
+    binary = ""
+    for c in msg:
+        binary += bin(ord(c))[2:]
+
+    # Designates the start of the message with sending 5 bits of 1
+    for i in range(0,4):
+        pyautogui.hotkey("altleft", "y")
+        i += 1
+
+    # Uses Zoom global shortcuts to toggle raise/lower hand for 1
+    # and mute/unmute for 0 every half second 
+    for b in binary:
+        if b == "1":
+            pyautogui.hotkey("altleft", "y")
+        if b == "0":
+            pyautogui.hotkey("altleft", "a")
+        time.sleep(.5)
+    
